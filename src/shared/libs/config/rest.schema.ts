382cf743a80445139ad0ea6d39c4d5ada9eae872
Yuriy <originalgamerdaddy@icloud.com>
2023-10-04 16:10:21 +0500

    updates rest schema

--- src/shared/libs/config/rest.schema.ts
+++ src/shared/libs/config/rest.schema.ts
@@ -7,6 +7,10 @@ export type RestSchema = {
   PORT: number;
   SALT: string;
   DB_HOST: string;
+  DB_USERNAME: string;
+  DB_PASSWORD: string;
+  DB_PORT: string;
+  DB_NAME: string;
 }
 
 export const configRestSchema = convict<RestSchema>({
@@ -14,18 +18,42 @@ export const configRestSchema = convict<RestSchema>({
     doc: 'Port for incoming connections',
     format: 'port',
     env: 'PORT',
-    default: 4000
+    default: 4000,
   },
   SALT: {
     doc: 'Salt for password hash',
     format: String,
     env: 'SALT',
-    default: null
+    default: null,
   },
   DB_HOST: {
     doc: 'IP address of the database server (MongoDB)',
     format: 'ipaddress',
     env: 'DB_HOST',
-    default: '127.0.0.1'
+    default: '127.0.0.1',
+  },
+  DB_USERNAME: {
+    doc: 'Username to connect to the db',
+    format: String,
+    env: 'DB_USERNAME',
+    default: null,
+  },
+  DB_PASSWORD: {
+    doc: 'Password to connect to the db',
+    format: String,
+    env: 'DB_PASSWORD',
+    default: null,
+  },
+  DB_PORT: {
+    doc: 'Port to connect to the db',
+    format: 'port',
+    env: 'DB_PORT',
+    default: '27017',
+  },
+  DB_NAME: {
+    doc: 'Db name',
+    format: String,
+    env: 'DB_NAME',
+    default: 'six-cities'
   },
 });
