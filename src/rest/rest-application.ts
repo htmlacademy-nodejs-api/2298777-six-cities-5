8b2846e3b94cf67c7d7a604d5f868f50b707b75d
Yuriy <originalgamerdaddy@icloud.com>
2023-10-23 23:30:02 +0400

    adds route interface

--- src/rest/rest-application.ts
+++ src/rest/rest-application.ts
@@ -4,15 +4,19 @@ import { injectable, inject } from 'inversify';
 import { Component } from '../shared/types/index.js';
 import { DbClient } from '../shared/libs/db-client/index.js';
 import { getMongoURI } from '../shared/helpers/index.js';
+import express, { Express } from 'express';
 
 @injectable()
 export class RestApplication {
+  private express: Express;
 
   constructor(
     @inject(Component.Logger) private readonly logger: Logger,
     @inject(Component.Config) private readonly config: Config<RestSchema>,
     @inject(Component.DBClient) private readonly dbClient: DbClient,
-  ) {}
+  ) {
+    this.express = express();
+  }
 
   private async initDB() {
     return await this.dbClient.connect(getMongoURI(
@@ -24,11 +28,19 @@ export class RestApplication {
     ));
   }
 
+  private async initServer() {
+    this.express.listen(this.config.get('PORT'));
+  }
+
   public async init() : Promise<void> {
     this.logger.info('Rest application init');
 
     this.logger.info('Init database');
     await this.initDB();
     this.logger.info('Init database completed');
+
+    this.logger.info('Init server');
+    await this.initServer();
+    this.logger.info(`Server started on localhost:${this.config.get('PORT')}`);
   }
 }
